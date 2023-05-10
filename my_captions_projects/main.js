46e952a2682f704d17819dfe4e4fe1dd41cfa642
RAJKUMAR R <132638055+raj2806n@users.noreply.github.com>
2023-05-10 16:06:33 +0530

    Add files via upload

---
+++ my_captions_projects/main.js
@@ -0,0 +1,17 @@
+// function loginPage(){
+//     window.location.href="Login.html";   
+// }
+const loginPage =()=>{
+    window.location.href="Login.html";
+}
+function mainPage(){
+  window.location.href="landingPage.html";
+  console.log("runned...");
+}
+function signUpPage(){
+  window.location.href="signup.html";
+}
+function mainContent(){
+  window.location.href="main.html";
+}
+// JavaScript code
